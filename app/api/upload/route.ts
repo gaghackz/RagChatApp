d42caac0246636bf12074d30be2b7928efa833b2
gaghackz <karanamgagan@gmail.com>
2025-09-27 20:02:25 +0530

    sending embeddings in response

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json({
       success: true,
-      text: extractedText,
+      text: embeddings,
     });
   } catch (err) {
     console.error("Upload Error:", err);
